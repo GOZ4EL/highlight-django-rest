33d2a26acc37f0b44d3362b4c410d13e80be0bf3
goz4el <hordemzerado@gmail.com>
2021-09-01 19:22:29 -0400

    Adding pagination.

--- highlight/settings.py
+++ highlight/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'highlight.wsgi.application'
 
 
+# Rest Configuration
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
